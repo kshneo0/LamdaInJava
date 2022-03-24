9da8f81635b85269c85194ee2c496f6e0bc89073
kshneo0 <kshneo@gmail.com>
2022-03-24 17:23:47 +0900

    Ch03Sec25

---
+++ src/Ch03/ThreadLambdas.java
@@ -0,0 +1,15 @@
+package Ch03;
+
+public class ThreadLambdas {
+	public static void main(String[] args) {
+
+		Runnable r = () -> performLongRunningOperation();
+		Thread t = new Thread(r);
+		t.start();
+
+	}
+
+	private static void performLongRunningOperation() {
+		System.out.println("This is a long running operation...");
+	}
+}
