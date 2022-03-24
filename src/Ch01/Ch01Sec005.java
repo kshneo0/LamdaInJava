635c467fabef7aaf5227ac18110e1b0528710de9
kshneo0 <kshneo@gmail.com>
2022-03-24 14:15:23 +0900

    Ch01Sec005

---
+++ src/Ch01/Ch01Sec005.java
@@ -0,0 +1,20 @@
+package Ch01;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Ch01Sec005 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> strings = Arrays.asList("Biggest", "at", "ate","I","apple","ball","cat","rhino","series");
+		List<String> orderedList = strings.stream()
+				.sorted(Comparator.comparing(String::length).thenComparing(Comparator.naturalOrder()))
+				.collect(Collectors.toList()); 
+		System.out.println(orderedList);
+
+	}
+
+}
