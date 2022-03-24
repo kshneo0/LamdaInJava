9782e2bedea5c74b39391ac8ec06e72ba8f88ea8
kshneo0 <kshneo@gmail.com>
2022-03-24 13:46:10 +0900

    Ch01Sec01

---
+++ src/Ch01/Ch01Sec01.java
@@ -0,0 +1,32 @@
+package Ch01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Ch01Sec01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> strings = Arrays.asList("Biggest", "at", "ate","I","apple","ball","cat","rhino","series");
+		Map<Integer, List<String>> lengthMap = new HashMap<>();
+		
+		for (String string : strings) {
+			List<String> sameLength = null;
+			Integer length = string.length();
+			
+			if(lengthMap.get(length) == null) {
+				sameLength = new ArrayList<> ();
+				lengthMap.put(length,  sameLength);
+			} else {
+				sameLength = lengthMap.get(length);
+			}
+			sameLength.add(string);
+		}
+		System.out.println(lengthMap);
+	}
+	
+
+}
