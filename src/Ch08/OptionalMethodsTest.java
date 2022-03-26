a1e72ce1cf0ef0d84aab9f678b1f02263ffcfc73
kshneo0 <kshneo@gmail.com>
2022-03-26 15:59:47 +0900

    Ch08Sec92

---
+++ src/Ch08/OptionalMethodsTest.java
@@ -0,0 +1,27 @@
+package Ch08;
+
+
+import java.util.Optional;
+
+public class OptionalMethodsTest {
+	
+	public static void main(String[] args) {
+
+		//use when we know it can never be null else NullPointer - bug indicator
+		Optional<String> str = Optional.of("Hello Streams"); // 
+
+		System.out.println(str.isPresent()); //checks for != null
+		
+		System.out.println(str.isEmpty());   //checks for == null
+				
+		//orElse
+		Optional<String> canBeEmpty = Optional.empty();
+		System.out.println(canBeEmpty.orElse("Lambdas"));
+		
+		//convenience method- uses 'empty' and 'of' together.
+		Optional<String> object = Optional.ofNullable(getObject());	// might return null
+		System.out.println(object.orElse("Streams"));
+					
+	}
+	
+}
